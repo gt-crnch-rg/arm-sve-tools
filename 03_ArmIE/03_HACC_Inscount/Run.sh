caa4b3d7c1c6aa72195d8e14e5189d609e35fecf
Filippo Spiga <spiga.filippo@gmail.com>
2019-07-09 17:24:19 +0800

    The option -only_from_app workls only if DynamoRIO is invoked directly

--- 03_ArmIE/03_HACC_Inscount/Run.sh
+++ 03_ArmIE/03_HACC_Inscount/Run.sh
@@ -5,5 +5,7 @@ make clean all
 
 armie -msve-vector-bits=128 -i libinscount_emulated.so -- ./HACCKernels
 
-armie -msve-vector-bits=128 -i libinscount_emulated.so --from-app-only -- ./HACCKernels
-
+# The option "-only_from_app" workls only if DynamoRIO is invoked directly
+export ARMIE_PATH=/opt/arm/arm-instruction-emulator-19.1_Generic-AArch64_RHEL-7_aarch64-linux
+export ARMIE_PATH=$(dirname `which armie`)/..
+$ARMIE_PATH/bin64/drrun -client $ARMIE_PATH/lib64/release/libsve_512.so 0 "" -client $ARMIE_PATH/samples/bin64/libinscount_emulated.so 1 "-only_from_app" -max_bb_instrs 32 -max_trace_bbs 4 -- ./HACCKernels
