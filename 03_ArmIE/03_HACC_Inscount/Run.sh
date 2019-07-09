d4effe29d15331b73dbb44d38c55b4c07d23c4e6
Filippo Spiga <spiga.filippo@gmail.com>
2019-07-09 17:30:07 +0800

    Script beautification

--- 03_ArmIE/03_HACC_Inscount/Run.sh
+++ 03_ArmIE/03_HACC_Inscount/Run.sh
@@ -3,9 +3,9 @@ make clean all
 
 # Run with ArmIE
 
-armie -msve-vector-bits=128 -i libinscount_emulated.so -- ./HACCKernels
+export EMULATED_SVE_LENGTH=128
+armie -msve-vector-bits=${EMULATED_SVE_LENGTH} -i libinscount_emulated.so -- ./HACCKernels
 
 # The option "-only_from_app" workls only if DynamoRIO is invoked directly
-export ARMIE_PATH=/opt/arm/arm-instruction-emulator-19.1_Generic-AArch64_RHEL-7_aarch64-linux
 export ARMIE_PATH=$(dirname `which armie`)/..
-$ARMIE_PATH/bin64/drrun -client $ARMIE_PATH/lib64/release/libsve_512.so 0 "" -client $ARMIE_PATH/samples/bin64/libinscount_emulated.so 1 "-only_from_app" -max_bb_instrs 32 -max_trace_bbs 4 -- ./HACCKernels
+$ARMIE_PATH/bin64/drrun -client $ARMIE_PATH/lib64/release/libsve_${EMULATED_SVE_LENGTH}.so 0 "" -client $ARMIE_PATH/samples/bin64/libinscount_emulated.so 1 "-only_from_app" -max_bb_instrs 32 -max_trace_bbs 4 -- ./HACCKernels
