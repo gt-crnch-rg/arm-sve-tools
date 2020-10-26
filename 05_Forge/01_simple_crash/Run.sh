e8c8dbcc1e25d66f2c9bb1a13d782c27d9cc9786
John Linford <john.linford@arm.com>
2020-10-26 10:27:39 -0500

    Delete Forge examples.

--- 05_Forge/01_simple_crash/Run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x #echo on
-
-make clean all
-mpirun --report-bindings --bind-to core -np 8 ./mmult1_f90.exe
-
-make clean all DEBUG=1
-ddt --connect mpirun --report-bindings --bind-to core -np 8 ./mmult1_f90.exe
--- 05_Forge/03_mda/Run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x #echo on
-
-make clean; make
-
-mpirun --report-bindings --bind-to core -n 4 ./imbpow.exe : -n 4 ./ovstr.exe
-
-ddt --connect mpirun --report-bindings --bind-to core -n 4 ./imbpow.exe : -n 4 ./ovstr.exe
-
